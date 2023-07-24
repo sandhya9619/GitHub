2086039b1fa9252d21aa25f1e40415f3337527f0
sandhya9619 <140065777+sandhya9619@users.noreply.github.com>
2023-07-24 16:45:46 +0530

    first

---
+++ C/star 2.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+  for( int i=0;i<5;i++)
+  {
+  	for(int j=1;j<=i;j++)
+  	
+  	
+  	{
+  		printf(" %d ", j%2);
+	  }
+  	
+  	printf("\n");
+  	
+  	
+  	
+  	
+  	
+  	
+  	
+	  }	
+	
+	
+	
+	
+	
+	
+	
+}
