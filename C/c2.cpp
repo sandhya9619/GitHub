64270ae7c7ce0efb1a961c84b556b457f737e331
sandhya9619 <140065777+sandhya9619@users.noreply.github.com>
2023-07-25 12:50:00 +0530

    first

---
+++ C/c2.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+main()
+{
+	int num1,num2;
+	
+    printf("enter a number1");
+    scanf("%d", &num1);
+    printf("enter number2");
+    scanf("%d",&num2);
+    
+	if (num1>num2)
+	
+	{ 
+	  printf(" num1 is greater ");
+	}
+
+    else
+    { 
+      printf("num2 is greater ");
+    }
+
+		
+	
+	
+	
+	
+	
+	
+	
+}
