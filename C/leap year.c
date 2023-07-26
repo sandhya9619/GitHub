9e3185766908e193cca7c9ee7fb0183ce2d3bde7
sandhya9619 <140065777+sandhya9619@users.noreply.github.com>
2023-07-26 15:43:17 +0530

    first

--- C/leap year.c
+++ C/leap year.c
@@ -5,13 +5,19 @@ int main ()
 	printf("leap year");
 	scanf("%d",&year);
 	
-	if(year%400==0){
-	
-	printf(" is a leap year");
-}
+	if(year%4==0)
+	{
+	   printf(" is a leap year");
+    
+	}
 	else if (year%100==0)
 	{
 	 printf("is not leap year");	
     }
-    return 0
+    else
+    { 
+    printf("is a not leap year");
+	}
+    return 0;
+    
 }
--- C/task 1 table.cpp
+++ C/task 1 table.cpp
@@ -4,7 +4,13 @@ int main ()
 	int i; 
 	for (i=1;i<=10;i++)
 	{
-		printf("1 * %d %d \n",i*1);
+		printf("1 %d=%d \n",i,i*1);
+		printf("2 %d=%d \n",i,i*2);
+		printf("3 %d=%d \n", i,i*3);
+		printf("4 %d=%d \n",i,i*4);
+		printf("5 %d=%d \n ",i,i*5);
+		printf("6 %d=%d \n", i, i*6);		
 	}
-	
+
+	return 0;
 } 
