3d62d95d843b986fbcca6183f5d8bdc5deeec4f8
sandhya9619 <140065777+sandhya9619@users.noreply.github.com>
2023-07-27 19:44:41 +0530

    first

---
+++ C/fibonaki M..cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+ {
+    int n, i;
+    
+    printf("Enter number of fibonaki ");
+    
+    scanf("%d", &n);
+    
+    printf("\n");
+    
+    for (i = 1; i <= n; i++) 
+	{
+        printf( " %d "fibonakis);
+    }
+    
+    printf("\n");
+    
+    return 0;
+}
