1b92da3d7c1059f6303f95a05e71c876be5e9828
sandhya9619 <140065777+sandhya9619@users.noreply.github.com>
2023-08-05 14:03:48 +0530

    first

--- C/.c.c
+++ C/.c.c
@@ -1,4 +1,26 @@
-#include<stdio.h>
+#include <stdio.h>
+
+int main() {
+    char name[] = "sandhya";
+    char birthdate[] = "25,jun 2003";
+    int age = 21;
+    char address[] = "gir somnath";
+
+    printf("1) Name: %s\n", name);
+    printf("2) Birthdate: %s\n", birthdate);
+    printf("3) Age: %d\n", age);
+    printf("4) Address: %s\n", address);
+
+    return 0;
+}
+//Replace "Your Name", "Your Birthdate", "Your Age", and "Your Address" with your actual information. The printf function allows you to format and display different types of data, such as strings and integers, using placeholders like %s for strings and %d for integers.
+
+
+
+
+
+
+//#include<stdio.h>
 
 	//int name;
 	//float birthdate;
@@ -21,53 +43,53 @@
 	//printf("%d%f%f%d\n",&name,&birthdate,&age,&address);
 	//return 0;
 	
-	int name()
-	{
-		int name;
-		printf("enter your name\n");
-		scanf("%d",&name);
+//	int name()
+	//{
+	//	int name;
+		//printf("enter your name\n");
+		//scanf("%d",&name);
 		 
-		 printf("%d\n",&name);
-	}
-	int age()
-	{
-		int age;
-		printf("enter your age\n");
-		scanf("%d",&age);
+		 //printf("%d\n",&name);
+	//}
+	//int age()
+	//{
+	//	int age;
+		//printf("enter your age\n");
+		//scanf("%d",&age);
 		
-		printf("%d\n",&age);
+		//printf("%d\n",&age);
 		
-	}
-	int birthdate()
-	{
-		int birthdate;
-		printf("enter your birthdate\n ");
-		scanf("%d",&birthdate);
+	//}
+	//int birthdate()
+	//{
+	//	int birthdate;
+	//	printf("enter your birthdate\n ");
+		//scanf("%d",&birthdate);
 		
-		printf("%d\n", &birthdate);
-	}
-	int address()
-	{
-		int address;
-		printf("enter your address");
-		scanf("%d",&address);
+		//printf("%d\n", &birthdate);
+	//}
+	//int address()
+	//{
+	//	int address;
+		//printf("enter your address");
+		//scanf("%d",&address);
 		
-		printf("%d\n",& address);
+		//printf("%d\n",& address);
 		
-	}
+	//}
 	
 
 
 
 
-int main()
-{
-	int address();
-	int birthdate();
-	int age();
-	int address();
+//int main()
+///{
+	//int address();
+	//int birthdate();
+	//int age();
+	//int address();
 	
-	return 0;
+	//return 0;
 	
 	
-}
+//}
