c337345bfad7f26c7f38fe4b08a2b07a0f76a094
sandhya9619 <140065777+sandhya9619@users.noreply.github.com>
2023-08-05 08:21:36 +0530

    first

---
+++ C/task 3.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+
+   int number1,number2;
+	printf("enter a number 1 : ");
+	scanf("%d",&number1);
+	
+	printf("enter a number 2 : ");
+	scanf("%d",&number2);
+	
+	if(number1>number2)
+	{
+	  printf("number1 is big number");
+    }
+    else
+    {
+    	printf("number 2 is big number");
+	}
+}
