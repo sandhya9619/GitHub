60bf333c6d83374bb998eb24b5a2065bab75beaf
sandhya9619 <140065777+sandhya9619@users.noreply.github.com>
2023-08-08 08:58:50 +0530

    first

---
+++ C/switch2.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+main()
+{
+	char charecter;
+	printf("enter a character ");
+	scanf("%c",&charecter);
+	
+	switch(charecter);
+	{
+		case 'a':
+			printf( charecter is vowel);
+			break;
+		case 'e':
+			printf("charecter is vowel");
+			break;
+			
+		case 'i':
+		printf("charecter is vowel");
+		break;
+		
+		case 'o':
+		printf("charecter is vowel");
+		break;
+		case 'u':
+		printf("charecter is vowel");
+		break;
+		
+		default;
+		printf("charecter is consenent");
+	}
+	
+	
+}
---
+++ C/switch3.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+main()
+{
+	int days;
+	printf("enter a days");
+	scanf("%d",&days);
+	
+	switch(days)
+	{
+		case 1:
+			printf("31 days");
+			break;
+			
+		case 2:
+			printf("28 days");
+			break;
+			
+		case 3:
+			printf("31 days");
+			break;
+			
+		case 4: 
+	     	printf("30 days");
+	    	break;
+		case 5:
+			printf("31 days");
+			break;
+		case 6:
+			printf("30 days");
+			break;
+		case 7:
+			printf("31 days");
+		    break;
+		case 8:
+			printf("31 days");
+		    break;
+		    
+		case 9:
+			printf("30 days");
+			break;
+		
+		case 10:
+		    printf("31 days");
+		    break;
+		    
+		case 11: 
+		    printf("30 days");
+		    break;
+		case 12:
+			printf("31 days");
+			break;
+			
+		default:
+		printf("invalid input");
+	}
+	
+	
+	
+	
+	
+	
+}
